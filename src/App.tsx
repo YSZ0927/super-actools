be9c9a6ad862f6ed0a437ef655382a9fd0175c4c
shuizeng Yu <shuizengyu@91jkys.com>
2020-10-14 21:00:25 +0800

    WIP:
    add antd style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from 'antd';
+import './app.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        asdfasd
+      <Button type="primary">Button</Button>
       </header>
     </div>
   );
